b288d945ba8b756a72b64401b083d783d69b89b0
Piotr Smarzynski <smarzony@gmail.com>
2020-12-16 18:34:53 +0100

    Radio is working. There is a bug with saving and getting from memory control type and channel, todo later.
    Most problems arise from bad power. Need to take extra caution on this.

--- Mobile_Robot_2018_transmitter/Mobile_Robot_2018_transmitter.ino
+++ Mobile_Robot_2018_transmitter/Mobile_Robot_2018_transmitter.ino
@@ -5,7 +5,7 @@ Author: Piotr Smarzyński
 
 #define NODEMCU
 #define WIFI_STATION
-#define CALIBRATE
+// #define CALIBRATE
 // #define WIFI_ACCESS_POINT
 // #define PROMINI
 
@@ -165,6 +165,120 @@ analogCorrection analog_correction;
 
 byte menu_no = 0;
 
+void prepareOutMessage()
+{
+  int value_send;
+
+  #ifdef NODEMCU
+    value_send = remoteAI.readADC_SingleEnded(ANALOG_LEFT_X )/ 4;
+    #ifdef CALIBRATE
+     value_send = value_send  + analog_correction.analog_left_X_correct;
+    #endif
+    message_transmit.analog_left_X = byte_limit(value_send);
+
+    value_send = remoteAI.readADC_SingleEnded(ANALOG_LEFT_Y )/ 4;
+    #ifdef CALIBRATE
+     value_send = value_send  + analog_correction.analog_left_Y_correct;
+    #endif
+    message_transmit.analog_left_Y = byte_limit(value_send);
+
+    value_send = remoteAI.readADC_SingleEnded(ANALOG_RIGHT_X )/ 4;
+    #ifdef CALIBRATE
+     value_send = value_send  + analog_correction.analog_right_X_correct;
+    #endif
+    message_transmit.analog_right_X = byte_limit(value_send);
+
+    value_send = remoteAI.readADC_SingleEnded(ANALOG_RIGHT_Y )/ 4;
+    #ifdef CALIBRATE
+     value_send = value_send  + analog_correction.analog_right_Y_correct;
+    #endif
+    message_transmit.analog_right_Y = byte_limit(value_send);
+  #endif
+
+  #ifdef PROMINI
+    value_send = analogRead(ANALOG_LEFT_X) / 4;
+    #ifdef CALIBRATE
+     value_send = value_send  + analog_correction.analog_left_X_correct;
+    #endif
+    message_transmit.analog_left_X = byte_limit(value_send);
+
+    value_send = analogRead(ANALOG_LEFT_Y) / 4;
+    #ifdef CALIBRATE
+     value_send = value_send  + analog_correction.analog_left_Y_correct;
+    #endif
+    message_transmit.analog_left_Y = byte_limit(value_send);
+
+    value_send = analogRead(ANALOG_RIGHT_X) / 4;
+    #ifdef CALIBRATE
+     value_send = value_send  + analog_correction.analog_right_X_correct;
+    #endif
+    message_transmit.analog_right_X = byte_limit(value_send);
+
+    value_send = analogRead(ANALOG_RIGHT_Y) / 4;
+    #ifdef CALIBRATE
+     value_send = value_send  + analog_correction.analog_right_Y_correct;
+    #endif
+    message_transmit.analog_right_Y = byte_limit(value_send);
+  #endif
+  
+
+  message_transmit.control_mode = control_mode;
+
+  message_transmit.potentiometer = analogRead(POTENTIOMETER) / 4;
+  message_transmit.rotory_encoder = rotory_encoder.value;
+
+  // Some magic to set bits in byte
+  #ifdef PROMINI
+    message_transmit.bit_array ^= (-digitalRead(SIDE_SWITCH) ^ message_transmit.bit_array) & (1UL << 0);
+  #endif
+  #ifdef NODEMCU
+    message_transmit.bit_array ^= (-remoteIO.digitalRead(SIDE_SWITCH) ^ message_transmit.bit_array) & (1UL << 0);
+  #endif
+  message_transmit.bit_array ^= (-analog_left_switch_state ^ message_transmit.bit_array) & (1UL << 1);
+  message_transmit.bit_array ^= (-analog_right_switch_state ^ message_transmit.bit_array) & (1UL << 2);
+  message_transmit.bit_array ^= (-rotory_encoder.switch_state ^ message_transmit.bit_array) & (1UL << 3);
+
+
+  outcoming_message[sizeof(outcoming_message) - 1] = message_counter;
+  message_transmit.message_no = message_counter;
+  message_counter++;
+
+  sendRadio();
+}
+
+void sendRadio()
+{
+  last_message_send = now;
+  radio.stopListening();
+  radio.write(&message_transmit, sizeof(message_transmit));
+  radio.startListening();
+}
+
+void readRadio(bool printing)
+{
+  if (radio.available())
+  {
+    radioData.last_message_no = radioData.current_message_no;
+    radio.read(&message_receive, sizeof(message_receive));
+    radioData.current_message_no = message_receive.message_no;
+    radioData.messages_lost = radioData.current_message_no - radioData.last_message_no - 1;
+    radioData.radio_not_availalble = 0;
+    radioData.radio_not_availalble_counter = 0;
+  }
+  else
+  {
+    radioData.radio_not_availalble = 1;
+    radioData.radio_not_availalble_counter++;
+  }
+
+  if (message_receive.velocity_measured_left == 0 && message_receive.velocity_measured_right &&
+      message_receive.message_no == 0)
+    radioData.empty_receive_data = 1;
+  else
+    radioData.empty_receive_data = 0;
+
+}
+
 void setup()
 {
 
@@ -216,7 +330,8 @@ void setup()
     analog_correction.analog_right_X_correct = int(get_memory(2, 1)) - 128;
     analog_correction.analog_right_Y_correct = int(get_memory(3, 1)) - 128;
   #endif
-  radio_channel = get_memory(4, 1);
+  // radio_channel = get_memory(4, 1);
+  radio_channel = 0;
   control_mode = get_memory(5, 1);
 
   //---------------------- Radio config BEGIN -----------------
@@ -242,6 +357,9 @@ void setup()
     calibration(analog_correction);
   #endif
   Serial.begin(115200);
+  delay(500);
+  Serial.println('radio_channel: '+String(radio_channel));
+  Serial.println('control_mode: '+String(control_mode));
 
 }
 
@@ -336,7 +454,7 @@ void loop()
         rotory_encoder.value_int = 0;
       if (rotory_encoder.value_int < 0)
         rotory_encoder.value_int = 120;
-      radio_channel = rotory_encoder.value_int;
+      // radio_channel = rotory_encoder.value_int;
       save_memory(4, 1, radio_channel);
       break;
 
@@ -359,8 +477,9 @@ void loop()
   if (now - SerialRawTimer > 500)
   {
     SerialRawTimer = now;
-    serialPrintTx(message_transmit);
+    // serialPrintTx(message_transmit);
     // serialPrintPCF(remoteIO);
+    // Serial.println("radio_connected: "+String(radio.isChipConnected()));
   }
 }
 
--- Mobile_Robot_2018_transmitter/RadioHandle.ino
+++ Mobile_Robot_2018_transmitter/RadioHandle.ino
@@ -1,5 +1,11 @@
 #include "Arduino.h"
 
+// #define ANALOG_LEFT_X 2
+// #define ANALOG_LEFT_Y 3
+// #define ANALOG_RIGHT_X 0
+// #define ANALOG_RIGHT_Y 1
+// #define NODEMCU
+
 uint8_t byte_limit(int value)
 {
   if (value > 255)
@@ -9,116 +15,116 @@ uint8_t byte_limit(int value)
   return uint8_t(value);
 }
 
-void prepareOutMessage()
-{
-  int value_send;
+// void prepareOutMessage(Adafruit_ADS1015 remoteAI, radioDataTrasnsmit message_transmit)
+// {
+//   int value_send;
 
-  #ifdef NODEMCU
-    value_send = remoteAI.readADC_SingleEnded(ANALOG_LEFT_X )/ 4;
-    #ifdef CALIBRATE
-     value_send = value_send  + analog_correction.analog_left_X_correct;
-    #endif
-    message_transmit.analog_left_X = byte_limit(value_send);
+//   #ifdef NODEMCU
+//     value_send = remoteAI.readADC_SingleEnded(ANALOG_LEFT_X )/ 4;
+//     #ifdef CALIBRATE
+//      value_send = value_send  + analog_correction.analog_left_X_correct;
+//     #endif
+//     message_transmit.analog_left_X = byte_limit(value_send);
 
-    value_send = remoteAI.readADC_SingleEnded(ANALOG_LEFT_Y )/ 4;
-    #ifdef CALIBRATE
-     value_send = value_send  + analog_correction.analog_left_Y_correct;
-    #endif
-    message_transmit.analog_left_Y = byte_limit(value_send);
+//     value_send = remoteAI.readADC_SingleEnded(ANALOG_LEFT_Y )/ 4;
+//     #ifdef CALIBRATE
+//      value_send = value_send  + analog_correction.analog_left_Y_correct;
+//     #endif
+//     message_transmit.analog_left_Y = byte_limit(value_send);
 
-    value_send = remoteAI.readADC_SingleEnded(ANALOG_RIGHT_X )/ 4;
-    #ifdef CALIBRATE
-     value_send = value_send  + analog_correction.analog_right_X_correct;
-    #endif
-    message_transmit.analog_right_X = byte_limit(value_send);
+//     value_send = remoteAI.readADC_SingleEnded(ANALOG_RIGHT_X )/ 4;
+//     #ifdef CALIBRATE
+//      value_send = value_send  + analog_correction.analog_right_X_correct;
+//     #endif
+//     message_transmit.analog_right_X = byte_limit(value_send);
 
-    value_send = remoteAI.readADC_SingleEnded(ANALOG_RIGHT_Y )/ 4;
-    #ifdef CALIBRATE
-     value_send = value_send  + analog_correction.analog_right_Y_correct;
-    #endif
-    message_transmit.analog_right_Y = byte_limit(value_send);
-  #endif
+//     value_send = remoteAI.readADC_SingleEnded(ANALOG_RIGHT_Y )/ 4;
+//     #ifdef CALIBRATE
+//      value_send = value_send  + analog_correction.analog_right_Y_correct;
+//     #endif
+//     message_transmit.analog_right_Y = byte_limit(value_send);
+//   #endif
 
-  #ifdef PROMINI
-    value_send = analogRead(ANALOG_LEFT_X) / 4;
-    #ifdef CALIBRATE
-     value_send = value_send  + analog_correction.analog_left_X_correct;
-    #endif
-    message_transmit.analog_left_X = byte_limit(value_send);
+//   #ifdef PROMINI
+//     value_send = analogRead(ANALOG_LEFT_X) / 4;
+//     #ifdef CALIBRATE
+//      value_send = value_send  + analog_correction.analog_left_X_correct;
+//     #endif
+//     message_transmit.analog_left_X = byte_limit(value_send);
 
-    value_send = analogRead(ANALOG_LEFT_Y) / 4;
-    #ifdef CALIBRATE
-     value_send = value_send  + analog_correction.analog_left_Y_correct;
-    #endif
-    message_transmit.analog_left_Y = byte_limit(value_send);
+//     value_send = analogRead(ANALOG_LEFT_Y) / 4;
+//     #ifdef CALIBRATE
+//      value_send = value_send  + analog_correction.analog_left_Y_correct;
+//     #endif
+//     message_transmit.analog_left_Y = byte_limit(value_send);
 
-    value_send = analogRead(ANALOG_RIGHT_X) / 4;
-    #ifdef CALIBRATE
-     value_send = value_send  + analog_correction.analog_right_X_correct;
-    #endif
-    message_transmit.analog_right_X = byte_limit(value_send);
+//     value_send = analogRead(ANALOG_RIGHT_X) / 4;
+//     #ifdef CALIBRATE
+//      value_send = value_send  + analog_correction.analog_right_X_correct;
+//     #endif
+//     message_transmit.analog_right_X = byte_limit(value_send);
 
-    value_send = analogRead(ANALOG_RIGHT_Y) / 4;
-    #ifdef CALIBRATE
-     value_send = value_send  + analog_correction.analog_right_Y_correct;
-    #endif
-    message_transmit.analog_right_Y = byte_limit(value_send);
-  #endif
+//     value_send = analogRead(ANALOG_RIGHT_Y) / 4;
+//     #ifdef CALIBRATE
+//      value_send = value_send  + analog_correction.analog_right_Y_correct;
+//     #endif
+//     message_transmit.analog_right_Y = byte_limit(value_send);
+//   #endif
   
 
-  message_transmit.control_mode = control_mode;
+//   message_transmit.control_mode = control_mode;
 
-  message_transmit.potentiometer = analogRead(POTENTIOMETER) / 4;
-  message_transmit.rotory_encoder = rotory_encoder.value;
+//   message_transmit.potentiometer = analogRead(POTENTIOMETER) / 4;
+//   message_transmit.rotory_encoder = rotory_encoder.value;
 
-  // Some magic to set bits in byte
-  #ifdef PROMINI
-    message_transmit.bit_array ^= (-digitalRead(SIDE_SWITCH) ^ message_transmit.bit_array) & (1UL << 0);
-  #endif
-  #ifdef NODEMCU
-    message_transmit.bit_array ^= (-remoteIO.digitalRead(SIDE_SWITCH) ^ message_transmit.bit_array) & (1UL << 0);
-  #endif
-  message_transmit.bit_array ^= (-analog_left_switch_state ^ message_transmit.bit_array) & (1UL << 1);
-  message_transmit.bit_array ^= (-analog_right_switch_state ^ message_transmit.bit_array) & (1UL << 2);
-  message_transmit.bit_array ^= (-rotory_encoder.switch_state ^ message_transmit.bit_array) & (1UL << 3);
+//   // Some magic to set bits in byte
+//   #ifdef PROMINI
+//     message_transmit.bit_array ^= (-digitalRead(SIDE_SWITCH) ^ message_transmit.bit_array) & (1UL << 0);
+//   #endif
+//   #ifdef NODEMCU
+//     message_transmit.bit_array ^= (-remoteIO.digitalRead(SIDE_SWITCH) ^ message_transmit.bit_array) & (1UL << 0);
+//   #endif
+//   message_transmit.bit_array ^= (-analog_left_switch_state ^ message_transmit.bit_array) & (1UL << 1);
+//   message_transmit.bit_array ^= (-analog_right_switch_state ^ message_transmit.bit_array) & (1UL << 2);
+//   message_transmit.bit_array ^= (-rotory_encoder.switch_state ^ message_transmit.bit_array) & (1UL << 3);
 
 
-  outcoming_message[sizeof(outcoming_message) - 1] = message_counter;
-  message_transmit.message_no = message_counter;
-  message_counter++;
+//   outcoming_message[sizeof(outcoming_message) - 1] = message_counter;
+//   message_transmit.message_no = message_counter;
+//   message_counter++;
 
-  sendRadio();
-}
+//   sendRadio();
+// }
 
-void sendRadio()
-{
-  last_message_send = now;
-  radio.stopListening();
-  radio.write(&message_transmit, sizeof(message_transmit));
-  radio.startListening();
-}
+// void sendRadio()
+// {
+//   last_message_send = now;
+//   radio.stopListening();
+//   radio.write(&message_transmit, sizeof(message_transmit));
+//   radio.startListening();
+// }
 
-void readRadio(bool printing)
-{
-  if (radio.available())
-  {
-    radioData.last_message_no = radioData.current_message_no;
-    radio.read(&message_receive, sizeof(message_receive));
-    radioData.current_message_no = message_receive.message_no;
-    radioData.messages_lost = radioData.current_message_no - radioData.last_message_no - 1;
-    radioData.radio_not_availalble = 0;
-    radioData.radio_not_availalble_counter = 0;
-  }
-  else
-  {
-    radioData.radio_not_availalble = 1;
-    radioData.radio_not_availalble_counter++;
-  }
+// void readRadio(bool printing)
+// {
+//   if (radio.available())
+//   {
+//     radioData.last_message_no = radioData.current_message_no;
+//     radio.read(&message_receive, sizeof(message_receive));
+//     radioData.current_message_no = message_receive.message_no;
+//     radioData.messages_lost = radioData.current_message_no - radioData.last_message_no - 1;
+//     radioData.radio_not_availalble = 0;
+//     radioData.radio_not_availalble_counter = 0;
+//   }
+//   else
+//   {
+//     radioData.radio_not_availalble = 1;
+//     radioData.radio_not_availalble_counter++;
+//   }
 
-  if (message_receive.velocity_measured_left == 0 && message_receive.velocity_measured_right &&
-      message_receive.message_no == 0)
-    radioData.empty_receive_data = 1;
-  else
-    radioData.empty_receive_data = 0;
+//   if (message_receive.velocity_measured_left == 0 && message_receive.velocity_measured_right &&
+//       message_receive.message_no == 0)
+//     radioData.empty_receive_data = 1;
+//   else
+//     radioData.empty_receive_data = 0;
 
-}
+// }
